92f63b46f4656a0e69c356bca002b8aebdd742a9
Michael Ebube <michaelebube18@gmail.com>
2023-03-07 23:45:38 +0100

    New Commit

--- app.js
+++ app.js
@@ -237,6 +237,225 @@ greetMorning('bobo')
 greetAfternoon('Elon')
 */
 
+/* forEach */
+  /*const people = [
+  {nameOf: 'Tommy', age: 25, position: 'developer'},
+  {nameOf: 'Arthur', age: 32, position: 'designer'},
+  {nameOf: 'John', age: 24, position: 'boss'},
+]
+
+function showPerson(person) {
+//console.log(person)
+console.log(person.position.toUpperCase());
+}
+
+//people.forEach(showPerson)
+people.forEach(function(item) {
+console.log(item.position.toUpperCase());
+}); */
+
+/* map */
+const people = [
+  {nameOf: 'Tommy', age: 25, position: 'developer'},
+  {nameOf: 'Arthur', age: 32, position: 'designer'},
+  {nameOf: 'John', age: 24, position: 'boss'},
+  {nameOf: 'Ada', age: 21, position: 'boss'},
+]
+
+const ages = people.map(function(person) {
+  //console.log(person);
+  return person.age + 20
+});
+const newPeople = people.map(function(person) {
+  return {
+    firstName: person.nameOf.toUpperCase(),
+    oldAge: person.age + 20,
+  
+  };
+});
+
+const burrito = people.map(function(person) {
+  return `<h1>${person.nameOf}</h1>`;
+
+})
+
+console.log(ages)
+console.log(newPeople)
+document.body.innerHTML = burrito.join('')
+console.log(burrito)
+
+/* Filter */  
+const peoples = [
+  {nameOf: 'Tommy', age: 25, position: 'developer'},
+  {nameOf: 'Arthur', age: 32, position: 'designer'},
+  {nameOf: 'John', age: 24, position: 'boss'},
+  {nameOf: 'Ada', age: 21, position: 'boss'},
+];
+const youngPeople = peoples.filter(function(person) {
+  return person.age <=25
+});
+
+const developers = peoples.filter(function(person) {
+  return person.position === 'developer'
+})
+console.log(youngPeople);
+console.log(developers)
+
+/* Find */
+const individuals = [
+  {nameOf: 'Tommy', age: 25, position: 'developer', id:1},
+  {nameOf: 'Arthur', age: 32, position: 'designer', id: 2},
+  {nameOf: 'John', age: 24, position: 'boss', id: 3},
+  {nameOf: 'Ada', age: 21, position: 'boss', id: 4},
+];
+
+const tortilla = ['Tommy', 'Arthur', 'John'];
+console.log(
+  tortilla.find(function(name) {
+  return name === 'Tommy'
+})
+);
+
+const individualss = individuals.find(function(person) {
+  return person.id === 3;
+});
+//console.log(individualss);
+console.log(individualss.nameOf);
+
+const individualss2 = individuals.filter(function(person) {
+  return person.id === 3;
+});
+
+console.log(individualss2[0].nameOf);
+
+/* Reduce */
+const folks = [
+  {nameOf: 'Tommy', age: 25, position: 'developer', id:1, salary: 200},
+  {nameOf: 'Arthur', age: 32, position: 'designer', id: 2, salary: 300},
+  {nameOf: 'John', age: 24, position: 'boss', id: 3, salary: 500},
+  {nameOf: 'Ada', age: 21, position: 'boss', id: 4, salary: 500},
+];
+
+const total = folks.reduce(function(acc, currItem) {
+  console.log(`total ${acc}`);
+  console.log(`current money ${currItem.salary}`);
+  acc += currItem.salary
+
+  return acc
+}, 500);
+
+console.log(total)
+
+/* Math Object */
+// Rounding Down
+// let number4 = 4.5678
+// let result4 = Math.floor(number4);
+// console.log(result4);
+
+let number4 = 4.5678;
+let result4 = Math.floor(8.9999);
+console.log(result4);
+
+//Rounding Up
+let number5 = 4.1222;
+let result5 = Math.ceil(number5);
+console.log(result5);
+
+//Square Root
+let number6 = 89;
+let result6 = Math.sqrt(number6);
+console.log(result6);
+
+//PI
+let result7= Math.PI
+console.log(result7);
+
+//Min
+let result8 = Math.min(47,67,8) 
+console.log(result8);
+
+//Max
+let result9 = Math.max(47,67,8) 
+console.log(result9);
+
+// Random Numbers
+//let result10 =Math.floor(Math.random() * 10)
+//console.log(result10);
+let result10 =Math.ceil(Math.random() * 10)
+console.log(result10); /* OR */
+let result11 =Math.floor(Math.random() * 10 + 1)
+//console.log(result10);
+
+
+/* Date Object */
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+const days = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+]
+
+const date = new Date()
+//const date = new Date('1/1/2020')
+
+const month = date.getMonth();
+console.log(months[month])
+
+const day = date.getDay()
+console.log(days[day]);
+
+console.log(date.getDate());
+console.log(date.getFullYear());
+
+const dateSentence = `${days[day]}, ${date.getDate()} ${months[month]} ${date.getFullYear()}. `
+
+console.log(dateSentence);
+
+document.body.innerHTML = dateSentence
+
+
+
+
+/* DOM */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
